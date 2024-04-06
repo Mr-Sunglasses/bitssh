f592ddba31a3ba040bbe397ca67a71f9c1d9c572
Sam <asamadans@gmail.com>
2024-04-07 03:52:32 +0530

    changed build backend back to setuptools, fixed some typing issues

--- src/bitssh/prompt.py
+++ src/bitssh/prompt.py
@@ -4,12 +4,14 @@ from .utils import ConfigPathUtility
 from .ui import UI
 import os
 
+from inquirer import List
+
 
 class Prompt:
     @classmethod
     def ask_host_prompt(cls) -> None:
-        HOST = ConfigPathUtility.get_config_file_host_data()
-        questions = [
+        HOST: list[str] = ConfigPathUtility.get_config_file_host_data()
+        questions: list[List] = [
             inquirer.List(
                 name="host",
                 message="Select the Host Given in the Above List",
@@ -19,9 +21,9 @@ class Prompt:
 
         answers = inquirer.prompt(questions=questions, theme=GreenPassion())
 
-        cmd = answers["host"]
-        cmd = cmd[6::]
-        cmd = f"ssh {cmd}"
+        cmd: str = answers["host"]
+        cmd: str = cmd[6::]
+        cmd: str = f"ssh {cmd}"
         UI.console.print(
             "Please Wait While Your System is Connecting to the Remote Server 🖥️",
             style="green",
